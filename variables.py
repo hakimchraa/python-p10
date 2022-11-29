a116113d9565447304f685df79aeb494c2adf7f6
hakim <akimchraa@gmail.com>
2022-11-29 11:46:37 +0100

    Ajout d'un cours sur les variables
    Création du fichier de cours.
    ajout de variables de type entier et chaine de caractere

---
+++ variables.py
@@ -0,0 +1,14 @@
+# l'operateur d'affectation = permet d'injecter une valeur 
+# dans une variable
+
+my_number1 = 123
+my_number2 = -42
+
+print (my_number1)
+print (my_number2)
+
+my_text1 = "ceci est une chaine de caracteres"
+my_text2 = "ceci est aussi une chaine de caracteres"
+
+print (my_text1)
+print (my_text2)
