a8eea177a238f21e4499d5e365840f09f9367522
hakim <akimchraa@gmail.com>
2022-12-05 10:55:31 +0100

    cours python sur les variables et leur type

--- hello.py
+++ hello.py
@@ -1,3 +1,4 @@
 #!/bin/python3
 
 print ("hello Python!")
+
--- variables.py
+++ variables.py
@@ -3,7 +3,25 @@
 
 my_number1 = 123
 my_number2 = -42
+my_number6 = 0
 
+print (my_number1)
+print (my_number2)
+print (my_number6)
+print(type(my_number1))
+print(type(my_number6))
+
+# float, normbre à virgule flottante
+
+my_number3 = 3.14
+my_number4 = -2.71
+# 0.0 ou 0. ou .0
+my_number5 = 0.0
+
+print (type(my_number3))
+print (my_number4)
+print (my_number5)
+print (type(my_number5))
 print (my_number1)
 print (my_number2)
 
